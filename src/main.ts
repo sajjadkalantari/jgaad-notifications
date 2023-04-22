00172a40c4e0abda0c618ef5f99ac579acc1df19
sajad <sajadkalantari@outlook.com>
2023-04-22 18:20:24 +0430

    add global exception handler

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { GlobalExceptionsFilter } from './utilities/dtos/global-exception.filter';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   await AppModule.setupSwagger(app); // add this line
+
+  app.useGlobalFilters(new GlobalExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/utilities/dtos/global-exception.filter.ts
@@ -0,0 +1,41 @@
+import { Catch, ExceptionFilter, HttpException, ArgumentsHost, HttpStatus, ValidationError } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { ResponseBase } from './response.dto';
+import { Error as MongooseValidationError } from 'mongoose';
+// import { ValidationError } from "mongoose";
+
+@Catch()
+export class GlobalExceptionsFilter implements ExceptionFilter {
+    catch(exception: any, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+
+        if (exception instanceof InvalidRequestException) {
+            // Handle custom type errors
+            const status = exception.getStatus();
+            response.status(status).json(ResponseBase.failed(...exception.errors));
+
+        }
+        else if (exception instanceof MongooseValidationError) {
+            response.status(HttpStatus.BAD_REQUEST).json(ResponseBase.failed(exception.message));
+        }
+        else {
+            console.error(exception);
+            response.status(500).json({
+                statusCode: 500,
+                message: 'Internal server error',
+                timestamp: new Date().toISOString(),
+                path: request.url,
+            });
+        }
+    }
+}
+
+export class InvalidRequestException extends HttpException {
+    errors: string[];
+    constructor(message: string, errors: ValidationError[]) {
+        super(message, HttpStatus.BAD_REQUEST);
+        this.errors = errors.map(e => `${e.property}: ${JSON.stringify(e.constraints)}`);
+    }
+}
