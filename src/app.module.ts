b361f6f630b32ec456f05a67b1e6ef4192c6eb61
sajad <sajadkalantari@outlook.com>
2023-04-22 18:20:03 +0430

    config mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { JwtModule } from '@nestjs/jwt';
 import { NotificationsModule } from './notifications/notifications.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot("mongodb://127.0.0.1:27017/jgaad"),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
@@ -28,7 +30,7 @@ export class AppModule {
       .build();
 
     const document = SwaggerModule.createDocument(app, options);
-    
+
     SwaggerModule.setup('docs', app, document);
   }
 }
