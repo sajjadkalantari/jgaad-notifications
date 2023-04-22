02d094331dff3a76e513aacdbf650a21bf36c1ee
sajad <sajadkalantari@outlook.com>
2023-04-22 09:52:13 +0430

    add and config swagger to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+
+  static async setupSwagger(app) {
+    
+    const options = new DocumentBuilder()
+      .setTitle('Jgaad API')
+      .setDescription('test notifications api')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    
+    SwaggerModule.setup('docs', app, document);
+  }
+}
