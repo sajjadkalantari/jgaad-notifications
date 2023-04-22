3169504b8da325416022c452eff2a6ae6bab712c
sajad <sajadkalantari@outlook.com>
2023-04-22 21:13:59 +0430

    add test for add notification method

--- src/notifications/test/notifications.service.spec.ts
+++ src/notifications/test/notifications.service.spec.ts
@@ -23,34 +23,49 @@ describe('NotificationsService', () => {
 
   it('should return valid notification', async () => {
     //Arrange
-    const expectedNotification = { email: 'test@example.com', notification: 'example notification' };
+    const expectedNotification = { email: 'test@example.com', notifications: ['example notification'] };
     jest.spyOn(notificationModel, 'findOne').mockReturnValue({
       lean: jest.fn().mockReturnValue({ exec: jest.fn().mockReturnValue(expectedNotification) }),
     } as any);
-    
+
     //Act
     const notification = await service.getNotifications("test@example.com");
-    
+
     //Assert
     expect(notification).toEqual(expectedNotification);
     expect(notificationModel.findOne).toBeCalledWith({ email: 'test@example.com' });
   });
 
-  it('should return not found exception', async () => {
+  it('should throw not found exception', async () => {
     //Arrange
     const expectedNotification = null;
     jest.spyOn(notificationModel, 'findOne').mockReturnValue({
       lean: jest.fn().mockReturnValue({ exec: jest.fn().mockReturnValue(expectedNotification) }),
     } as any);
-    
-    
+
+
     //Act & Assert
     await expect(service.getNotifications("test@example.com")).rejects.toThrow("user notifications not found");
-    
+
+  });
+
+
+  it('should add new notification & return', async () => {
+    //Arrange
+    const newNotification = { email: 'test2@example.com', notifications: ['example2 notification'] };
+    jest.spyOn(notificationModel, 'create').mockReturnValue(newNotification as any);
+
+    //Act
+    const notification = await service.addNotification(newNotification);
+
+    //Assert
+    expect(notification).toEqual(newNotification);
+    expect(notificationModel.create).toBeCalledWith(newNotification);
   });
 });
 
 
 const mockNotificationModel = {
   findOne: jest.fn(),
+  create: jest.fn(),
 };
