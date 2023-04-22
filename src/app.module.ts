057dffde04a144d0c9e4cea3ee351420dfb2d821
sajad <sajadkalantari@outlook.com>
2023-04-22 09:59:36 +0430

    add dotenv to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'your-secret-key-here',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
     })
   ],
